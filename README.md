4bea89a5687f56416ffddcc06599528e0a2f27cb
David Goffredo <dmgoffredo@gmail.com>
2023-01-07 14:16:05 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+waybackinator.com
+=================
+This is a thin wrapper around the [Wayback Machine][1]'s [API][2].
+
+Append a URL as the path to `waybackinator.com`, e.g.
+```
+https://waybackinator.com/https://news.ycombinator.com
+```
+and the response will be either a link to the most recent saved version of the
+corresponding site, or an error.
+
+Alternatively, `waybackinator.com` hosts a simple search form.  Go to:
+```
+https://waybackinator.com
+```
+
+[1]: https://en.wikipedia.org/wiki/Wayback_Machine
+[2]: https://archive.org/help/wayback_api.php
---
+++ waybackinator.js
@@ -0,0 +1,184 @@
+const http = require('node:http');
+const https = require('node:https');
+const process = require('node:process');
+const querystring = require('node:querystring');
+
+const httpsAgent = new https.Agent({keepAlive: true});
+
+// url -> {archiveURL: string, error: string}
+function fetchArchiveURL(url, callback) {
+    const requestOptions = {
+        agent: httpsAgent,
+        protocol: 'https:',
+        hostname: 'archive.org',
+        path: `/wayback/available?${querystring.encode({url})}`
+    };
+
+    https.request(requestOptions)
+    .on('response', response => {
+        const status = response.statusCode;
+        if (status >= 300) {
+            return callback({error: `archive.org replied with status ${status} ${response.statusMessage}`});
+        }
+
+        let body = '';
+        response
+        .on('data', chunk => body += chunk)
+        .on('end', () => {
+            console.log('archive.org response body: ', body);
+            try {
+                // const {available, url} = JSON.parse(body).archived_snapshots.closest;
+                const snapshots = JSON.parse(body).archived_snapshots;
+                const snapshot = snapshots.closest;
+                if (!snapshot || !snapshot.available) {
+                    return callback({error: 'That URL is not available via the internet archive API.'});
+                }
+                return callback({archiveURL: snapshot.url});
+            } catch (error) {
+                console.error(error);
+                return callback({error: `Error occurred processing archive.org response:\n${body}`});
+            }
+        });
+    })
+    .on('error', error => {
+        return callback({error: error.toString()});
+    })
+    .end();
+}
+
+function escapeForHTML(raw) {
+    return raw.replace(/["<&]/, char => ({
+        '"': '&quot;',
+        '<': '&lt;',
+        '&': '&amp;'
+    }[char]));
+}
+
+// LRU cache
+const cache = (() => {
+    const evictionMilliseconds = 1000 * 60 * 60;
+    const maxNumEntries = 1024 * 32;
+    const entries = {}; // input url -> {output url, Date.now() when added}
+    let numEntries = 0; // number of elements in `entries`
+    // singly-linked list for LRU eviction
+    // Each element is {url, addedTime, next}
+    let additionHead; // first element in singly-linked list for LRU eviction
+    let additionTail; // last element in singly-linked list for LRU eviction
+    return {
+        // url -> archiveURL | undefined
+        lookup: url => {
+          const now = Date.now();
+          const entry = entries[url];
+          if (entry === undefined) {
+            return;
+          }
+
+          const {archiveURL, addedTime} = entry;
+          if (now - addedTime >= evictionMilliseconds) {
+              delete entries[url];
+              --numEntries;
+              return;
+          }
+
+          return archiveURL;
+        },
+        // (inputURL, archiveURL) -> undefined
+        set: (inputURL, archiveURL) => {
+          const now = Date.now();
+          while (numEntries === maxNumEntries) {
+              const {url, addedTime, next} = additionHead;
+              const entry = entries[url];
+              if (entry.addedTime === addedTime) {
+                  delete entries[url];
+                  --numEntries;
+              }
+              additionHead = next;
+          }
+          entries[inputURL] = {archiveURL, addedTime: now};
+          const addition = {url: inputURL, addedTime: now};
+          if (!additionHead) {
+              additionHead = addition;
+          } else if (additionTail) {
+              additionTail.next = addition;
+              additionTail = addition;
+          } else {
+              additionTail = addition;
+          }
+          ++numEntries;
+        }
+    };
+})();
+
+function getArchiveURL(url, callback) {
+  // Check the cache.
+  let archiveURL = cache.lookup(url);
+  if (archiveURL !== undefined) {
+    console.log(`retrieved ${url} from cache`);
+    return callback({archiveURL});
+  }
+
+  return fetchArchiveURL(url, callback);
+}
+
+function onRequest (request, response) {
+  let url = request.url.slice(1); // without leading "/"
+  if (url.slice(-1) === '?') {
+    // Empty HTML form might leave a trailing empty query.
+    url = url.slice(0, -1);
+  }
+  console.log('Received request for url: ', url);
+  
+  getArchiveURL(url, ({archiveURL, error}) => {
+      if (error !== undefined) {
+        deliverError(response, error);
+      } else {
+        cache.set(url, archiveURL);
+        deliverSuccess(response, archiveURL);
+      }
+  });
+}
+
+function deliverSuccess(response, archiveURL) {
+  response.setHeader('Content-Type', 'text/html');
+  response.writeHead(200);
+  const escapedURL = escapeForHTML(archiveURL);
+  const html = `<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>waybackinator</title>
+  </head>
+  <body>
+   <p><a href="${escapedURL}">${escapedURL}</a></p>
+  </body>
+</html>
+`;
+  response.end(html);
+}
+
+function deliverError(response, error) {
+  response.setHeader('Content-Type', 'text/html');
+  response.writeHead(404);
+  const escapedError = escapeForHTML(error);
+  const html = `<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>waybackinator</title>
+  </head>
+  <body>
+   <p>${escapedError}</p>
+  </body>
+</html>
+`;
+  response.end(html);
+}
+
+const host = '0.0.0.0';
+const port = 8000;
+const server = http.createServer(onRequest);
+server.listen({host, port}).on('listening', () => console.log(`Listening on ${host}:${port}`));
+
+process.on('SIGTERM', function () {
+  server.close(() => process.exit(0));
+});
